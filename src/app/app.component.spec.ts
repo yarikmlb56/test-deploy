7bdd27ddb03e1e441c47225b35ba7d832322079b
Viktoria Voloshchuk <viktoria_voloshchuk@epam.com>
2019-11-25 18:21:23 +0200

    configure tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -10,22 +10,14 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  // it('should create the app', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.debugElement.componentInstance;
-  //   expect(app).toBeTruthy();
-  // });
-  //
-  // it(`should have as title 'test-deploy'`, () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.debugElement.componentInstance;
-  //   expect(app.title).toEqual('test-deploy');
-  // });
-  //
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.debugElement.nativeElement;
-  //   expect(compiled.querySelector('.content span').textContent).toContain('test-deploy app is running!');
-  // });
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it('title should have correct value', () => {
+    const component = new AppComponent();
+    expect(component.title).toBe('title');
+  });
 });
