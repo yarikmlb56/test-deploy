9c1222beefa7ec92c687eb67b952694eaf364554
Viktoria Voloshchuk <viktoria_voloshchuk@epam.com>
2019-11-27 14:54:01 +0200

    fix lintning errors

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,9 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+import { Environment } from '../interfaces/environment';
 
-export const environment: object = {
+export const environment: Environment = {
   production: false
 };
 
