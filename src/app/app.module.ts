af9907352bff1c594bd23bb6022834cc954049e6
Viktoria Voloshchuk <viktoria_voloshchuk@epam.com>
2019-11-28 11:49:12 +0200

    add header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { appRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
 import { CoreModule } from './modules/core/core.module';
+import { HomeModule } from './modules/home/home.module';
+import { SignInModule } from './modules/signin/signin.module';
+import { SignUpModule } from './modules/signup/signup.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +16,9 @@ import { CoreModule } from './modules/core/core.module';
     appRoutingModule,
     BrowserModule,
     CoreModule,
+    HomeModule,
+    SignInModule,
+    SignUpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,15 +1,22 @@
 import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 
+import { SignInComponent } from './modules/signin/components/sign-in.component';
+import { SignUpComponent } from './modules/signup/components/sign-up.component';
+
 const routes: Routes = [
+  // {
+  //   path: '',
+  //   redirectTo: '/home',
+  //   pathMatch: 'full'
+  // },
   {
-    path: '',
-    redirectTo: '/home',
-    pathMatch: 'full'
+    path: 'sign-in',
+    component: SignInComponent
   },
   {
-    path: 'home',
-    loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
+    path: 'sign-up',
+    component: SignUpComponent
   }
 ];
 
